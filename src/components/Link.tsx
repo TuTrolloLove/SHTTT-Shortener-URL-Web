a8531a800ebdf3900a4ae9822a1dad07f82f0bbd
Jose Manuel Bono Campos <122403351+BonoStudent@users.noreply.github.com>
2024-06-29 13:49:21 +0200

    Delete unussed component and add emoji to better visual link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -9,7 +9,8 @@ interface IProps {
     <>
 
       <html>
-        <p id="pLink" style={{fontFamily:"Bebas Neue, sans-serif", fontWeight:"400", fontStyle:"normal", marginBottom:"1.3rem", textAlign:"center", color:"white" }}>{shortURL}</p>
+    <br></br>
+        <p id="pLink" style={{fontFamily:"Bebas Neue, sans-serif", fontWeight:"400", fontStyle:"normal", marginBottom:"1.3rem", textAlign:"center", color:"white", border:"1px solid blue", backgroundColor:"rgb(2, 2, 155)"}}>{shortURL}</p>
         </html>
   </>
     );
