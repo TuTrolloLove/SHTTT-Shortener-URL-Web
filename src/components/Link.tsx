6b1513671bc0bb5168dbef61d55133f72fd75c6f
Jose Manuel Bono Campos <122403351+BonoStudent@users.noreply.github.com>
2024-06-29 14:01:00 +0200

    Deleted background color and border from Link.tsx

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -10,7 +10,7 @@ interface IProps {
 
       <html>
     <br></br>
-        <p id="pLink" style={{fontFamily:"Bebas Neue, sans-serif", fontWeight:"400", fontStyle:"normal", marginBottom:"1.3rem", textAlign:"center", color:"white", border:"1px solid blue", backgroundColor:"rgb(2, 2, 155)"}}>{shortURL}</p>
+        <p id="pLink" style={{fontFamily:"Bebas Neue, sans-serif", fontWeight:"400", fontStyle:"normal", marginBottom:"1.3rem", textAlign:"center", color:"white"}}>{shortURL}</p>
         </html>
   </>
     );
